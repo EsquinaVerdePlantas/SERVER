63a1611ac6fafa5b011183599605dd46c482746f
Jhonattan Escorihuela <136757234+JhonattanEscorihuela@users.noreply.github.com>
2023-12-06 15:56:11 -0500

    creacion de relacion plantas vs categorias

--- src/controllers/Plants/createIndoorPlants.js
+++ src/controllers/Plants/createIndoorPlants.js
@@ -1,15 +1,31 @@
 const { default: mongoose } = require('mongoose');
 let fs = require('node:fs/promises');
-require('../../schemas/plantsSchema')
+require('../../schemas/plantsSchema');
+require('../../schemas/plantsCategorySchema');
 
 
 let Plantas = mongoose.model('PlantsModel');
+let PlantsCategoryModel = mongoose.model('PlantsCategoryModel');
 
 let createMasivoIndoorPlants = async () => {
-    fs.readFile('../SERVER/src/utils/indoorPlants.json', 'utf-8')
+
     try {
         let data = await fs.readFile('../SERVER/src/utils/indoorPlants.json', 'utf-8');
-        let indoorPlants = await Plantas.insertMany(JSON.parse(data));
+        let plantsData = JSON.parse(data);
+        let categories = await PlantsCategoryModel.find({});
+
+        let plantsToInsert = plantsData.map(plant => {
+            // Encuentra la categoría correspondiente en base al nombre en el JSON
+            let category = categories.find(cat => cat.name === plant.plantCategory);
+
+            return {
+                ...plant,
+                plantsCategory: category._id // Asigna el ID de la categoría correspondiente
+            };
+        });
+
+        // Insertar las plantas con sus categorías asociadas
+        let indoorPlants = await Plantas.insertMany(plantsToInsert);
         return indoorPlants;
     } catch (error) {
         console.error('Error al leer o crear las plantas Indoor:', error);
@@ -17,5 +33,7 @@ let createMasivoIndoorPlants = async () => {
     }
 }
 
-module.exports = createMasivoIndoorPlants;
+
+
+module.exports = { createMasivoIndoorPlants };
 
--- src/handlers/Plants/createIndoorPlantsHandler.js
+++ src/handlers/Plants/createIndoorPlantsHandler.js
@@ -1,13 +1,13 @@
-let createIndoorPlants = require('../../controllers/Plants/createIndoorPlants');
+let { createMasivoIndoorPlants } = require('../../controllers/Plants/createIndoorPlants');
 
 
-let createMasivoIndoorPlants = async (req, res) => {
+let createMasivoIndoorPlantsHandler = async (req, res) => {
     try {
-        let response = await createIndoorPlants();
+        let response = await createMasivoIndoorPlants();
         res.status(201).json({ message: 'Plantas de interior creadas correctamente', data: response });
     } catch (error) {
         console.error('Error al crear las plantas Indoor:', error);
     }
 }
 
-module.exports = createMasivoIndoorPlants;
+module.exports = createMasivoIndoorPlantsHandler;
